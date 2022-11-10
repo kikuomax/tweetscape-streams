652ff4522febc69e9c22743da4c525614a99f642
Kikuo Emoto <kemoto@codemonger.io>
2022-11-10 18:22:12 +0900

    chore(indexer): rename stack name
    - Renames the CDK stack name: "CdkStack" → "tweetscape-${deploymentStage}"
      where $deploymentStage is substituted with the deployment stage of a
      CDK stack.

--- indexer/cdk/lib/cdk-stack.ts
+++ indexer/cdk/lib/cdk-stack.ts
@@ -1,16 +1,15 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+
+import type { DeploymentStage } from './deployment-stage';
+
+interface Props extends cdk.StackProps {
+  /** Deployment stage. */
+  deploymentStage: DeploymentStage;
+};
 
 export class CdkStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+  constructor(scope: Construct, id: string, props: Props) {
     super(scope, id, props);
-
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'CdkQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
   }
 }
